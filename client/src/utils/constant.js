e026fa5ee3c45b83e5ec05e831818795f2164688
rohit <duttarohit754@gmail.com>
2024-10-08 23:52:36 +0530

    trial

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:3000/api/v1/user" || "https://job-portal-final-1.onrender.com";
-export const JOB_API_END_POINT="http://localhost:3000/api/v1/job" || "https://job-portal-final-1.onrender.com";
-export const APPLICATION_API_END_POINT="http://localhost:3000/api/v1/application" || "https://job-portal-final-1.onrender.com";
-export const COMPANY_API_END_POINT="http://localhost:3000/api/v1/company" || "https://job-portal-final-1.onrender.com";
+export const USER_API_END_POINT= "https://job-portal-final-1.onrender.com";
+export const JOB_API_END_POINT= "https://job-portal-final-1.onrender.com";
+export const APPLICATION_API_END_POINT="https://job-portal-final-1.onrender.com";
+export const COMPANY_API_END_POINT= "https://job-portal-final-1.onrender.com";
