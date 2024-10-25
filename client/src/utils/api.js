af8ac802912299b84cb453044d16e4bad84bb8e6
rohit <duttarohit754@gmail.com>
2024-10-25 12:06:54 +0530

    api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,10 @@
 
 import axios from 'axios';
 import { getToken } from './authUtils';
-import { USER_API_END_POINT } from '@/utils/constant'
+import { API_END_POINT } from '@/utils/constant'
 
 const api = axios.create({
-  baseURL: USER_API_END_POINT,
+  baseURL: API_END_POINT,
 });
 
 api.interceptors.request.use(
